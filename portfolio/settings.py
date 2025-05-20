b82093dcd3ec8ccff9d3fbbf27ada462d3ce7083
Saran24875 <s.r2005jsus@gmail.com>
2025-05-20 15:03:00 +0530

    Fix DATABASE_URL environment variable retrieval and update database configuration

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,9 +80,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-database_url = os.environ . get( "DATABASE URL")
+database_url = os.getenv("DATABASE_URL")
 DATABASES['default'] = dj_database_url.parse(database_url)
+print(repr(os.getenv("DATABASE_URL")))
 
 
 # Password Validation
